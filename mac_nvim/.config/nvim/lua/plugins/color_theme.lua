194ba77526120a1595e8324f85d136ca6567d6f7
ft_linux <zhangfengtao1998@gmail.com>
2025-11-24 21:21:57 +0000

    added mac nvim

---
+++ mac_nvim/.config/nvim/lua/plugins/color_theme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "catppuccin/nvim",
+  },
+  lazy = false,
+  name = "catppuccin",
+  {
+    "lazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
+-- return {
+--   {
+--     "catppuccin/nvim",
+--   },
+--   lazy = false,
+--   name = "catppuccin",
+--   {
+--     "lazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin-mocha",
+--     },
+--   },
+-- }
