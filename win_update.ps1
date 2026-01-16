c5130027a5305e727b5edd1fba1403ce2ce95fb8
mn19fz <zhangfengtao1998@gmail.com>
2026-01-16 16:18:37 +0000

    Finalised the win_update script On branch office_pc Changes to be committed: 	modified:   win_update.ps1

--- win_update.ps1
+++ win_update.ps1
@@ -1,6 +1,7 @@
 # This is the PowerShell script for updating the config on Windows.
 echo "I am currently in $(pwd)"
 
+# ==> Scoop related section
 # Check if scoop.sh is installed and add extras to bucket
 echo "Checking is scoop is installed"
 if (-Not (get-command scoop))
@@ -9,8 +10,13 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
 
-echo "Updating scoop buckets"
-scoop bucket add extras
+# Added buckets extras if not added
+echo "Check the status of scoop and buckets"
+$buckets=$(ls $HOME/scoop/buckets)
+if (-Not ($buckets -contains "extras"))
+{
+    scoop bucket add extras
+}
 scoop update
 
 $packages = @(
@@ -31,26 +37,33 @@ $packages = @(
 'zoxide'
 )
 
+# Install mising packages if 
+$installed=$(ls $HOME/scoop/apps)
 for (($i=0); $package = $packages[$i]; $i++)
 {
-    # echo $package
-    scoop install $package
-    scoop update $package
-}
-
-# Add glazewm to startup folder
-$startup_glazewm="$HOME/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/glazewm.exe"
-$glazewm_scoop="$HOME/scoop/apps/glazewm/current/glazewm.exe"
-if (-Not(test-path $startup_glazewm))
-{
-    if (test-path $glazewm_scoop)
+    if (-Not ($installed -contains "$package"))
     {
-        clear
-        echo Hello
-        Copy-Item -Force $glazewm_scoop $startup_glazewm
+        echo "Installing Package: $package"
+        scoop install $package
     }
-}
+    else
+    {
+        scoop update $package
+    }
 
+}
+# <== Scoop related section
+
+# ==> Added startup app section
+# Add wt and glazewm to startup folder
+$startup_dir="$HOME/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/"
+$glazewm="$HOME/scoop/apps/glazewm/current/glazewm.exe"
+Copy-Item -Force -Recurse $glazewm $startup_dir
+
+
+# <== Added startup app section
+
+# ==> Configuration synchronizing section
 # Start synchronize configuration files
 echo "Synchronizing Windows configuration"
 
@@ -65,14 +78,12 @@ $config_dir = $(ls ./win_home/config/)
 for (($i=0); $config=$config_dir[$i]; $i++)
 {
     Copy-Item -Force -Recurse ./win_home/config/$config $HOME/.config/
-    # if (test-path $HOME/.config/$config)
-    # {
-    #     echo yes
-    # }
 }
 
 # Glazewm
 Copy-Item -Force -Recurse ./win_home/glazewm $HOME/.glzr/
 
 echo "Synchronzed all windows configurations."
+# <== Configuration synchronizing section
+
 echo "Happy coding!"
