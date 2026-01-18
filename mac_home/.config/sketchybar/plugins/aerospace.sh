1fba2b18e9cec2862a8bd2d6bd5db31c72dad672
Fengtao Zhang <zhangfengtao1998@gmail.com>
2026-01-18 00:47:43 +0000

    Started the sketchybar config     Create all spaces to center,     check if the workspace exist, and create at the left with order.     highlight the focused. On branch ft_mac Changes to be committed: 	modified:   mac_home/.config/aerospace/aerospace.toml 	modified:   mac_home/.config/sketchybar/plugins/aerospace.sh 	new file:   mac_home/.config/sketchybar/plugins/colors.sh 	modified:   mac_home/.config/sketchybar/sketchybarrc

--- mac_home/.config/sketchybar/plugins/aerospace.sh
+++ mac_home/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "$1" = "$FOCUSED_WORKSPACE" ]]; then
-	sketchybar --set $NAME background.drawing=on
-else
-	sketchybar --set $NAME background.drawing=off
-fi
+aerospace_workspaces=$(aerospace list-workspaces --all)
+focused=$(aerospace list-workspaces --focused)
+for workspace in "${aerospace_workspaces[@]}"; do
+	if [[ "$1" = "$FOCUSED_WORKSPACE" ]]; then
+		sketchybar --set $NAME background.drawing=on
+		sketchybar --set $NAME label.drawing=on
+		sketchybar --move $NAME before chevron
+	fi
+done
---
+++ mac_home/.config/sketchybar/plugins/colors.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export BAR=0x3f0ce6e6
+# Everforest theme
+export FOREGROUND=0xffd3c6aa
+export RED=0xffe6e80
+export YELLOW=0xffdbbc7f
+export GREEN=0xffa7c080
+export BLUE=0xff7fbbb3
+export PURPLE=0xffd699b6
+export AQUA=0xff83c092
+export ORANGE=0xffe69875
+export BACKGROUND=0xff1e2326
+export WHITE=0xffffffff
+export BLACK=0xff000000
+export BLACK0=0xff374145
+export BLACKBLUE=0xff384b55
+export BLACKGREEN=0xff3c4841
