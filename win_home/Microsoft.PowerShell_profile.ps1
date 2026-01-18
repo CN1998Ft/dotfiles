97bc3ed6f1ad338af2b34b8a81a027ec24bcc6a4
H_win_fengtao <zhangfengtao1998@gmail.com>
2026-01-18 19:35:26 +0000

    Fix printing hello at startup of powershell

--- win_home/Microsoft.PowerShell_profile.ps1
+++ win_home/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,6 @@
 # ==> region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 If (Test-Path "$HOME/miniforge3/Scripts/conda.exe") {
-    echo hello
     (& "$HOME/miniforge3/Scripts/conda.exe" "shell.powershell" "hook") |
     Out-String | ?{$_} | Invoke-Expression
 }
