4a0ef85c538312d57c4daf92e380f0490e02655c
H_win_fengtao <zhangfengtao1998@gmail.com>
2026-01-21 23:46:30 +0000

    Use default aerospace-sketchybar interaction     tell ghostty to open new window On branch home_win Changes to be committed: 	modified:   mac_home/.config/aerospace/aerospace.toml 	modified:   mac_home/.config/sketchybar/plugins/aerospace.sh 	modified:   mac_home/.config/sketchybar/sketchybarrc

--- mac_home/.config/sketchybar/plugins/aerospace.sh
+++ mac_home/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-total=$(aerospace list-workspaces --all --count)
-sketchybar --set $NAME label="$FOCUSED_WORKSPACE / $total"
+if [[ "$1" = "$FOCUSED_WORKSPACE" ]]; then
+    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME label.color=$BLACK
+else
+    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME label.color=$BLACKGREEN
+fi
+
+# total=$(aerospace list-workspaces --all --count)
+# sketchybar --set $NAME label="$FOCUSED_WORKSPACE / $total"
+
 # if [[ "$1" == "$FOCUSED_WORKSPACE" ]]; then
 # 	sketchybar --set $NAME label=""
 # else
