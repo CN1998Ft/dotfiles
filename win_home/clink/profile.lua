bd704e5ae898f5fb6167424740d3090ed7e98980
H_win_fengtao <zhangfengtao1998@gmail.com>
2026-07-07 15:22:07 +0100

    expand ~ for clink cmd

--- win_home/clink/profile.lua
+++ win_home/clink/profile.lua
@@ -8,8 +8,27 @@ os.setalias("nvide", "Neovide.exe $*")
 os.setalias("ls", "eza -h --group-directories-first --icons=auto $*")
 os.setalias("lua", "luajit $*")
 os.setalias("open", "start $*")
--- this cd will ruin normal cd use z instead
--- os.setalias("cd", 'for /f "delims=" %i in (\'zoxide query $*\') do @cd /d "%i"')
+os.setalias(
+	"msvc",
+	[["C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\VsDevCmd.bat" -arch=x64 -host_arch=x64 ]]
+)
+-- Expand ~ to %USERPROFILE%
+local function tilde_expander(command_line)
+	-- Check if the command starts with 'cd ' (case-insensitive)
+	local cmd_str = tostring(command_line)
+	if string.match(cmd_str, "^cd%s+") and string.match(cmd_str, "~") then
+		-- Replace '~' with the USERPROFILE environment variable
+		local user_profile = os.getenv("USERPROFILE")
+		if user_profile then
+			local new_command = string.gsub(command_line, "~", user_profile)
+			return new_command
+		end
+	end
+	-- Return nil if we didn't modify the command
+	return nil
+end
+-- Register the filter with Clink
+clink.oncommand(tilde_expander)
 
 -- borrow git usr stuff for windows usage
 local git_usr_bin = os.getenv("USERPROFILE") .. "\\scoop\\apps\\git\\current\\usr\\bin"
