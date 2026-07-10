bc133eb26ef4d84bf6a38bddd3d33a518af476e6
Fengtao Zhang <zhangfengtao1998@gmail.com>
2026-07-10 17:43:07 +0100

    add macos vim okular review

--- c_config/nvim/lua/plugins/text_editing.lua
+++ c_config/nvim/lua/plugins/text_editing.lua
@@ -2,7 +2,11 @@ vim.pack.add({
   "https://github.com/lervag/vimtex",
   "https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
-vim.g.vimtex_view_general_viewer = "okular"
+if vim.fn.has("win32") == 1 then
+  vim.g.vimtex_view_general_viewer = "okular"
+elseif vim.fn.has("mac") == 1 then
+  vim.g.vimtex_view_general_viewer = "/Applications/okular.app/Contents/MacOS/okular"
+end
 vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
 require("render-markdown").setup({
   enabled = false,
